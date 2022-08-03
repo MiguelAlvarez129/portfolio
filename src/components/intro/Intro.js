4d1fba12fed69cc8c5e0772fb61961b29e5eccbc
miguelalvarez129 <miguelalvarezunexpo@gmail.com>
2022-08-03 07:53:12 -0400

    work in progress animation

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -3,34 +3,39 @@ import { Box, Grid, GridItem, Center, Heading } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import blueGrid from '../../assets/img/blue-grid.svg'
 const Intro = () => {
-  return (
-    <Center h={'100vh'}>
 
-          <motion.div animate={{backgroundColor:['#ce285800','#ce2858'],height:[150,150,150],width:[1000,150,150],rotate:[0,0,360],borderRadius:['5px','5px','50%']}} transition={{delay:2,duration:2}} >
-            <Grid column={1} position={'relative'} top={[0,-100]} maxW={['fit-content','fit-content','none']}>
-              <GridItem colStart={1} rowStart={1}> 
-              <motion.div animate={{opacity:[0,1,0,1,0,1,0]}} transition={{duration: 2, ease:'easeInOut'}}>
-                  <Heading letterSpacing={'5px'} fontSize={'clamp(4rem,6vw,7rem)'} color={'#ce2858c2'} position={'relative'} zIndex={0}>
-                  Miguel Alvarez
-                  </Heading>
-              </motion.div>
-              </GridItem>
-              <GridItem colStart={1} rowStart={1}>
-              <motion.div animate={{opacity:[0,1,0,1,0,1,0]}} transition={{duration: 2, ease:'easeInOut'}}>
-                  <Heading letterSpacing={'5px'} fontSize={'clamp(4rem,6vw,7rem)'}  color={'#ffffffc2'} position={'relative'} zIndex={10} left={'clamp(0.8rem,0.8vw,1rem);'} top={'clamp(0.2rem,0.2vw,0.4rem)'}>
-                  Miguel Alvarez
-                </Heading>
-              </motion.div>
-              </GridItem>
-            </Grid>
-            <Box>
-            <Heading  fontSize={'clamp(4rem,6vw,7rem)'} backgroundClip={'text'} bgImage={`url(${blueGrid})`} color={'transparent'} position={'absolute'} top={'75px'}>
-              M
-              </Heading>
-            </Box>
-          </motion.div>
+  const variants = {
+    visible:{ 
+      x:['-33%','0%','-33%'],
+      textShadow:['0 0px 0px rgba(255,255,255,255)','0 15px 40px rgb(255,255,255)','0 0px 0px rgba(0,0,0,255)'],
+      transition:{
+        
  
-    </Center>
+      }
+    },
+  
+  }
+  const parent = {
+    visible:{
+      transition:{staggerChildren:.5, delayChildren:.5, repeat:'Infinity',ease:'easeInOut',duration: 3}
+    }
+  }
+
+  const letter = 'Miguel'
+  return (
+    <Center h={'100vh'} bg='blue'>
+
+     <motion.div  variants={parent} animate={'visible'} >
+        {letter.split('').map((e,i)=>(
+          <motion.div variants={variants} style={{color:'#000331',fontSize:'10rem', textShadow:'0 15px 40px rgb(0,0,0)',fontWeight:"bolder",display:'inline-block'}}> 
+            {e}
+          </motion.div>
+        ))}
+      </motion.div>
+
+      </Center>
+      
+
   )
 }
 
